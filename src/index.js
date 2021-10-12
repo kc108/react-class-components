9ef8ec179533ddc671e74b429260609a35f690d8
Kim Carpico <kcarpico@gmail.com>
2021-10-12 15:37:02 -0700

    updated state and app lifecycle walkthrough prct

--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from "react-dom";
 
 // This Allows us to pull in a bunch of functions from 'React.Component' into our class, in this case the 'App Class'.
 class App extends React.Component {
-  render() {
+  // The very first fnct to run, any time an instance is create, it will be called before anything else
+  constructor(props) {
+    // Required: This super() is needed to set up a fnct for us
+    super(props);
+
+    // This is our State Object
+    // use null when we do not know what it is initially
+    // *** THIS IS THE ONLY TIME WE DO A DIRECT ASSIGNMENT TO 'THIS.STATE' ***
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
       // success callback
-      (position) => console.log(position),
+      (position) => {
+        // We called setState!!!
+        this.setState({ lat: position.coords.latitude });
+        // //never setState to something else like this
+        // // this.state.lat = position.coords.latitude
+        // ** One exception: when we Initialize are State in the render function
+      },
       // failure callback
       (err) => console.log(err)
     );
+  }
 
-    return <div>Latitude: </div>;
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
