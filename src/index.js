ace581942c89b55044f50d682e9a19d5e91b7eea
Kim Carpico <kcarpico@gmail.com>
2021-10-17 14:43:55 -0700

    configured season to DRY code, added icons and added stylesheet for SeasonDisplay.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+
+import SeasonDisplay from "./SeasonDisplay";
 
 // This Allows us to pull in a bunch of functions from 'React.Component' into our class, in this case the 'App Class'.
 class App extends React.Component {
   // The very first fnct to run, any time an instance is create, it will be called before anything else
-  constructor(props) {
-    // Required: This super() is needed to set up a fnct for us
-    super(props);
+  // //constructor(props) {
+  //   // Required: This super() is needed to set up a fnct for us
+  //   // super(props);
 
-    // This is our State Object
-    // use null when we do not know what it is initially
-    // *** THIS IS THE ONLY TIME WE DO A DIRECT ASSIGNMENT TO 'THIS.STATE' ***
-    this.state = { lat: null, errorMessage: "" };
+  //   // This is our State Object
+  //   // use null when we do not know what it is initially
+  //   // *** THIS IS THE ONLY TIME WE DO A DIRECT ASSIGNMENT TO 'THIS.STATE' ***
+  //   // this.state = { lat: null, errorMessage: "" };
+  // // }
 
+  // Refactored code from above
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       // success callback
-      (position) => {
-        // We called setState!!!
-        this.setState({ lat: position.coords.latitude });
-        // //never setState to something else like this
-        // // this.state.lat = position.coords.latitude
-        // ** One exception: when we Initialize are State in the render function
-      },
+      (position) => this.setState({ lat: position.coords.latitude }),
       // failure callback
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
@@ -35,7 +35,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
